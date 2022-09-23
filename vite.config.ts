871d9864f1a5568aa1e4e79cae87776e14bb9e46
Petar Zarkov <p.zarkov@draftkings.com>
2022-09-23 09:21:16 +0300

    position-conveyor-properly

--- vite.config.ts
+++ vite.config.ts
@@ -4,42 +4,43 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    // Load app-level env vars to node-level env vars.
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  return {
-    base: "/the-biscuit-machine/",
-    build: {
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            "react": ["react"],
-            "react-dom": ["react-dom"],
-            "react-router-dom": ["react-router-dom"],
-            "@chakra-ui/react": ["@chakra-ui/react"],
-            "@chakra-ui/icons": ["@chakra-ui/icons"],
-            "react-icons": ["react-icons"],
-            "react-icons/bs": ["react-icons/bs"],
-            "react-icons/gi": ["react-icons/gi"],
-          }
-        }
-      }
-    },
-    resolve: {
-      alias: {
-        "@animations": path.resolve(__dirname, "./src/animations"),
-        "@components": path.resolve(__dirname, "./src/components"),
-        "@contracts": path.resolve(__dirname, "./src/contracts"),
-        "@hooks": path.resolve(__dirname, "./src/hooks"),
-        "@screens": path.resolve(__dirname, "./src/screens"),
-        "@config": path.resolve(__dirname, "./src/config"),
-        "@theme": path.resolve(__dirname, "./src/theme"),
-        "@store": path.resolve(__dirname, "./src/store"),
-      }
-    },
-    plugins: [
-      react()
-    ]
-  };
+    return {
+        base: "/the-biscuit-machine/",
+        build: {
+            emptyOutDir: true,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        "react": ["react"],
+                        "react-dom": ["react-dom"],
+                        "react-router-dom": ["react-router-dom"],
+                        "@chakra-ui/react": ["@chakra-ui/react"],
+                        "@chakra-ui/icons": ["@chakra-ui/icons"],
+                        "react-icons": ["react-icons"],
+                        "react-icons/bs": ["react-icons/bs"],
+                        "react-icons/gi": ["react-icons/gi"],
+                        "framer-motion": ["framer-motion"]
+                    }
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                "@animations": path.resolve(__dirname, "./src/animations"),
+                "@components": path.resolve(__dirname, "./src/components"),
+                "@contracts": path.resolve(__dirname, "./src/contracts"),
+                "@hooks": path.resolve(__dirname, "./src/hooks"),
+                "@screens": path.resolve(__dirname, "./src/screens"),
+                "@config": path.resolve(__dirname, "./src/config"),
+                "@theme": path.resolve(__dirname, "./src/theme"),
+                "@store": path.resolve(__dirname, "./src/store"),
+            }
+        },
+        plugins: [
+            react()
+        ]
+    };
 });
