c896b261568bd9cecb72dddf7961726502fa5acc
Petar Zarkov <p.zarkov@draftkings.com>
2022-09-23 08:48:32 +0300

    start-adding-animations-with-framer-motion

--- src/animations/Conveyor.tsx
+++ src/animations/Conveyor.tsx
@@ -1,16 +1,61 @@
 
 import React from "react";
-import { motion } from "framer-motion";
-import { useIteration } from "@hooks";
+import { motion, MotionStyle } from "framer-motion";
+
+export const ConveyorCircle: React.FC<{ motionStyle?: MotionStyle }> = ({ motionStyle }) => {
+    return (
+        <motion.div
+            style={{
+                width: 125,
+                height: 125,
+                borderRadius: "50%",
+                border: "5px dashed red",
+                ...motionStyle
+            }}
+            animate={{
+                rotate: [0, 180, 180]
+            }}
+            transition={{
+                duration: 5,
+                ease: "easeInOut",
+                repeat: Infinity
+            }}
+        />
+    );
+};
 
 export const Conveyor = () => {
-    const i = useIteration(0.1);
 
     return (
         <motion.div
-            initial={{ opacity: 0, scale: 0.5 * i }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-        />
+            style={{
+                width: 600,
+                height: 200,
+                borderRadius: "50%",
+                border: "5px dashed green"
+            }}
+            animate={{
+                rotate: [0, 180, 180, 0]
+            }}
+            transition={{
+                duration: 5,
+                ease: "easeInOut",
+                repeat: Infinity
+            }}
+        >
+            <div>
+                <ConveyorCircle motionStyle={{
+                    marginRight: "auto",
+                    marginLeft: 0
+                }}/>
+                <ConveyorCircle motionStyle={{
+                    margin: "auto"
+                }}/>
+                <ConveyorCircle motionStyle={{
+                    marginLeft: "auto",
+                    marginRight: 0
+                }}/>
+            </div>
+        </motion.div>
     );
 };
