c297229a97a706611db10068abb587b2b8c390c5
Petar Zarkov <p.zarkov@draftkings.com>
2022-09-26 13:12:09 +0300

    dynamic-conveyor-and-cookie-test

--- src/screens/BiscuitMachine.tsx
+++ src/screens/BiscuitMachine.tsx
@@ -1,12 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, LegacyRef } from "react";
 import { Box } from "@chakra-ui/react";
 import { Conveyor } from "@animations";
+import { useSize } from "@chakra-ui/react-use-size";
 
 export const BiscuitMachine: FC = () => {
+    const elementRef = React.useRef<LegacyRef<HTMLDivElement>>();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const dimensions = useSize(elementRef as unknown as HTMLDivElement | null);
 
     return (
-        <Box maxW={400}>
-            <Conveyor />
+        <Box
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            ref={elementRef as unknown as LegacyRef<HTMLDivElement>}
+            width={[260, 460, 660]}
+        >
+            <Conveyor w={dimensions?.width} />
         </Box>
     );
 };
+
