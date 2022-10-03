1cf39ec50e7a6b8dbb915ca78088d013ad0659e4
Petar Zarkov <p.zarkov@draftkings.com>
2022-10-03 08:26:56 +0300

    attempt-to-fix-on-animation-complete-on-mobile

--- src/animations/Stamper.tsx
+++ src/animations/Stamper.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useAnimationProvider } from "@hooks";
 
 // I really cannot draw
@@ -9,42 +9,44 @@ export const Stamper: React.FC<{ x: number }> = ({ x }) => {
     const [pausedAt, setPausedAt] = React.useState(0);
 
     return (
-        <motion.div
-            style={{
-                padding: 0,
-                margin: 0,
-                width: 50,
-                height: 50,
-                marginLeft: (x / 3) - 50,
-                y: isPaused ? pausedAt : 0
-            }}
-            animate={!isRunning || isPaused ? {} : isStamping ? {
-                y: 50,
-                transition: {
-                    ease: "linear"
-                }
-            } : {
-                y: 0,
-                transition: {
-                    ease: "linear"
-                }
-            }}
-            onUpdate={(latest) => {
-                if (!isPaused) {
-                    setPausedAt(latest.y as number);
-                }
-            }}
-            onAnimationComplete={() => {
-                setIsStamping(false);
-            }}
-        >
-            <Image
-                src={"images/stamper.png"}
+        <AnimatePresence>
+            <motion.div
                 style={{
                     padding: 0,
-                    margin: 0
+                    margin: 0,
+                    width: 50,
+                    height: 50,
+                    marginLeft: (x / 3) - 50,
+                    y: isPaused ? pausedAt : 0
                 }}
-            />
-        </motion.div>
+                animate={!isRunning || isPaused ? {} : isStamping ? {
+                    y: 50,
+                    transition: {
+                        ease: "linear"
+                    }
+                } : {
+                    y: 0,
+                    transition: {
+                        ease: "linear"
+                    }
+                }}
+                onUpdate={(latest) => {
+                    if (!isPaused) {
+                        setPausedAt(latest.y as number);
+                    }
+                }}
+                onAnimationComplete={() => {
+                    setIsStamping(false);
+                }}
+            >
+                <Image
+                    src={"images/stamper.png"}
+                    style={{
+                        padding: 0,
+                        margin: 0
+                    }}
+                />
+            </motion.div>
+        </AnimatePresence>
     );
 };
